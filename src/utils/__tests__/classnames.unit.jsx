bf75d5cc21d04d583c13468ab767925bd9774b75
zhouhua.tang <zhouhua.tang@shopee.com>
2021-04-25 23:23:25 +0800

    添加几个classNames的测试用例

--- src/utils/__tests__/classnames.unit.jsx
+++ src/utils/__tests__/classnames.unit.jsx
@@ -1,16 +1,24 @@
 import classNames from "../classnames";
 
 describe('classnames', () => {
-  it('接受一个参数', () => {
+  it('接受1个参数', () => {
     const result = classNames('a');
     expect(result).toEqual('a');
   })
-  it('接受两个参数', () => {
-    const result = classNames('a','b');
+  it('接受2个参数', () => {
+    const result = classNames('a', 'b');
     expect(result).toEqual('a b');
   })
-  it('其中一个参数是undefined', () => {
-    const result = classNames('a',undefined);
+  it('接受其中一个参数是undefined', () => {
+    const result = classNames('a', undefined);
     expect(result).toEqual('a');
+  })
+  it('接受0个参数', () => {
+    const result = classNames();
+    expect(result).toEqual('');
+  })
+  it('接受一些奇怪的参数', () => {
+    const result = classNames('name', '中文', null, undefined, false);
+    expect(result).toEqual('name 中文');
   })
 })
