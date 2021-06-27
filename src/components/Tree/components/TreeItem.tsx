3bc23bcace1a0f1143042e444420d7f2a27d026e
zhouhua.tang <zhouhua.tang@shopee.com>
2021-06-27 09:44:14 +0800

    重构TreeItem组件

---
+++ src/components/Tree/components/TreeItem.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { ChangeEventHandler, useState } from 'react';
+import Icon from '../../Icon';
+import { TreeDataItem, TreeProps } from '../index';
+import scopedClassMaker from '../../../utils/scopedClassMaker';
+
+type TreeItemProps = {
+  item: TreeDataItem;
+  level: number;
+  treeProps: TreeProps;
+};
+const sc = scopedClassMaker('react-ui-tree');
+
+const TreeItem: React.FC<TreeItemProps> = (props) => {
+  const { item, level, treeProps, ...rest } = props;
+  const { selected, multiple, onChangeSelected } = treeProps;
+  const [folded, setFolded] = useState(false);
+  const classes = {
+    [`level-${level}`]: true,
+    item: true,
+  };
+  const checked = multiple
+    ? selected.indexOf(item.value) >= 0
+    : selected === item.value;
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.checked) {
+      multiple
+        ? // @ts-ignore
+          onChangeSelected([...selected, item.value])
+        : // @ts-ignore
+          onChangeSelected(item.value);
+    } else {
+      multiple
+        ? // @ts-ignore
+          onChangeSelected(selected.filter((value) => value !== item.value))
+        : // @ts-ignore
+          onChangeSelected('');
+    }
+  };
+  const unfoldChildren = () => {
+    setFolded(false);
+  };
+  const foldChildren = () => {
+    setFolded(true);
+  };
+
+  return (
+    <div key={item.value} className={sc(classes)} {...rest}>
+      <div className={sc('text')}>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        {item.text}
+        {item.children &&
+          (folded ? (
+            <Icon
+              name={'unfold'}
+              className={sc('fold-icon')}
+              onClick={unfoldChildren}
+            />
+          ) : (
+            <Icon
+              name={'fold'}
+              className={sc('fold-icon')}
+              onClick={foldChildren}
+            />
+          ))}
+      </div>
+      <div className={sc({ fold: folded })}>
+        {item.children?.map((subItem) => (
+          <TreeItem
+            key={subItem.value}
+            item={subItem}
+            level={level + 1}
+            treeProps={treeProps}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default TreeItem;
--- src/components/Tree/index.tsx
+++ src/components/Tree/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import './index.scss';
-import scopedClassMaker from '../../utils/scopedClassMaker';
-import { ChangeEventHandler, useState } from 'react';
-import Icon from '../Icon';
+import TreeItem from './components/TreeItem';
 
 export interface TreeDataItem {
   text: string;
@@ -22,9 +20,8 @@ type SingleSelected = {
   multiple?: false;
   onChangeSelected: (newSelected: string) => void;
 };
-type Props = PartialProps & (MultipleSelected | SingleSelected);
-const sc = scopedClassMaker('react-ui-tree');
-const Tree: React.FC<Props> = (props) => {
+export type TreeProps = PartialProps & (MultipleSelected | SingleSelected);
+const Tree: React.FC<TreeProps> = (props) => {
   const {
     children,
     dataSource,
@@ -34,63 +31,12 @@ const Tree: React.FC<Props> = (props) => {
     ...rest
   } = props;
 
-  const RenderItem = (item: TreeDataItem, level: number) => {
-    const [folded, setFolded] = useState(false);
-    const classes = {
-      [`level-${level}`]: true,
-      item: true,
-    };
-    const checked = multiple
-      ? selected.indexOf(item.value) >= 0
-      : selected === item.value;
-    const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-      if (e.target.checked) {
-        multiple
-          ? // @ts-ignore
-            onChangeSelected([...selected, item.value])
-          : // @ts-ignore
-            onChangeSelected(item.value);
-      } else {
-        multiple
-          ? // @ts-ignore
-            onChangeSelected(selected.filter((value) => value !== item.value))
-          : // @ts-ignore
-            onChangeSelected('');
-      }
-    };
-    const unfoldChildren = () => {
-      setFolded(false);
-    };
-    const foldChildren = () => {
-      setFolded(true);
-    };
-
-    return (
-      <div key={item.value} className={sc(classes)} {...rest}>
-        <div className={sc('text')}>
-          <input type="checkbox" checked={checked} onChange={handleChange} />
-          {item.text}
-          {item.children &&
-            (folded ? (
-              <Icon
-                name={'unfold'}
-                className={sc('fold-icon')}
-                onClick={unfoldChildren}
-              />
-            ) : (
-              <Icon
-                name={'fold'}
-                className={sc('fold-icon')}
-                onClick={foldChildren}
-              />
-            ))}
-        </div>
-        <div className={sc({ fold: folded })}>
-          {item.children?.map((subItem) => RenderItem(subItem, level + 1))}
-        </div>
-      </div>
-    );
-  };
-  return <div>{dataSource?.map((item) => RenderItem(item, 0))}</div>;
+  return (
+    <div {...rest}>
+      {dataSource?.map((item) => (
+        <TreeItem key={item.value} item={item} level={0} treeProps={props} />
+      ))}
+    </div>
+  );
 };
 export default Tree;
